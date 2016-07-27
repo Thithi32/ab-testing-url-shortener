9a889bf704e8137d64f3c37fa41eaf06e4fca9db
Thierry Delbart <thierry@thde.pro>
2016-07-27 16:12:08 -0300

    Adding port in config

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ var config = {};
 
 config.webhost = process.env.WEBHOST || 'http://localhost:3000/';
 config.db = process.env.MONGODB_URI || 'mongodb://localhost/url_shortener';
+config.port = process.env.PORT || 3000;
 
 module.exports = config;
